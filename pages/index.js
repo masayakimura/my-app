ae945f8df84676c0dcb7e055e1fc5b5de3dc2b1c
masayakimura <masa_kuanoo@live.jp>
2021-01-10 01:52:24 +0900

    tomoerai

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          TOMOPE <a href="https://nextjs.org">Next.js!</a>
+          TOMOERAI <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
